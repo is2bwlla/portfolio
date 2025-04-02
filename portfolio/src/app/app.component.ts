83d02bc2965886ef278abc5a99425107761b95ba
is2bwlla <lucyrose22198@gmail.com>
2025-04-01 21:32:35 -0300

    tools

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
-  imports: [],//[RouterOutlet]
+  imports: [CommonModule], //[RouterOutlet]
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'portfolio';
+
+  isVisible: string | null = null;
+
+  toggleVisibility(menu: string) {
+    this.isVisible =  this.isVisible === menu ? null : menu;
+  }
 }
